fe18d8c1f8aa095c775b048e3913cbacd3926c9f
Kirill <qSatan1s@yandex.ru>
2021-01-09 00:31:23 +0300

    login end

---
+++ components/SingleComponents/Button.vue
@@ -0,0 +1,28 @@
+<template>
+  <button class="btn" type="submit">{{ button.text }}</button>
+</template>
+
+<script>
+export default {
+  props: ['button'],
+}
+</script>
+
+<style lang="scss" scoped>
+.btn {
+  width: inherit;
+  height: 44px;
+  cursor: pointer;
+  font-weight: 500;
+  font-size: 16px;
+  color: white;
+  background: #d6073d;
+  border-radius: 10px;
+}
+
+.btn:hover {
+  background: #be0536;
+  box-shadow: -6px -6px 16px rgba(255, 255, 255, 0.5),
+    0px 4px 4px rgba(0, 0, 0, 0.25);
+}
+</style>
---
+++ pages/Login.vue
@@ -0,0 +1,73 @@
+<template>
+  <main class="login-block">
+    <img src="@/assets/images/logo.svg" alt="popso" />
+    <form class="login_auth" @submit.prevent="submitHandler">
+      <input
+        v-model="controls.login"
+        placeholder="Логин"
+        class="login_auth-input"
+        type="text"
+        required
+      />
+      <input
+        v-model="controls.password"
+        placeholder="Пароль"
+        class="login_auth-input"
+        type="text"
+        required
+      />
+      <app-button :button="button" />
+    </form>
+  </main>
+</template>
+
+<script>
+import AppButton from '@/components/SingleComponents/Button'
+export default {
+  components: {
+    AppButton,
+  },
+  data: () => ({
+    controls: {
+      login: '',
+      password: '',
+    },
+    button: { text: 'Войти' },
+  }),
+  methods: {
+    submitHandler() {
+      this.$router.push('/')
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.login-block {
+  display: flex;
+  flex-flow: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+.login_auth {
+  margin-top: 60px;
+  display: flex;
+  flex-flow: column;
+
+  &-input {
+    border: 1px solid #cbcbcb;
+    border-radius: 10px;
+    margin: 8px 0;
+    width: 358px;
+
+    color: #9496a6;
+    padding: 12px 20px;
+  }
+}
+@media (max-width: 360px) {
+  .login_auth-input {
+    width: 296px;
+  }
+}
+</style>
